c1d5ce4b0893b315a28cba9cf5910d265e896634
Amir <60965542+amir4rab@users.noreply.github.com>
2024-05-02 16:37:30 -0400

    ♻️ Minor bug fixes

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./base.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <main className="prose dark:prose-invert py-12 mx-auto">
+  <main className="prose dark:prose-invert max-w-3xl p-8 pb-36 md:px-0 md:py-12 mx-auto">
     <React.StrictMode>
       <App />
     </React.StrictMode>
--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -5,70 +5,60 @@ import typography from "@tailwindcss/typography";
 export default {
   content: ["./index.html", "./src/**/*.tsx"],
   theme: {
-    theme: {
-      transitionTimingFunction: {
-        elastic: "cubic-bezier(0.68, 0, 0.32, 1.2)",
-      },
+    extend: {
       fontFamily: {
         sans: [
+          "Ubuntu",
           "system-ui",
           "-apple-system",
           "BlinkMacSystemFont",
           "Segoe UI",
           "Roboto",
           "Oxygen",
-          "Ubuntu",
           "Cantarell",
           "Open Sans",
           "Helvetica Neue",
           "sans-serif",
         ],
-        mono: [
-          "SF Mono",
-          "SFMono-Regular",
-          "ui-monospace",
-          "DejaVu Sans Mono",
-          "Menlo",
-          "Consolas",
-          "monospace",
-        ],
+        serif: ["Playfair Display", "serif"],
       },
-      extend: {
-        keyframes: {
-          "opacity-keyframes": {
-            "0%": {
-              opacity: 0,
-            },
-            "100%": {
-              opacity: 1,
-            },
+      transitionTimingFunction: {
+        elastic: "cubic-bezier(0.68, 0, 0.32, 1.2)",
+      },
+      keyframes: {
+        "opacity-keyframes": {
+          "0%": {
+            opacity: 0,
           },
-          "loading-keyframes": {
-            from: {
-              opacity: 0.5,
-            },
-            to: {
-              opacity: 1,
-            },
+          "100%": {
+            opacity: 1,
           },
         },
-        animation: {
-          "fade-in": "opacity-keyframes .5s ease-in-out forwards",
-          loading: "loading-keyframes infinite",
-        },
-        screens: {
-          standalone: {
-            raw: "(display-mode: standalone)",
+        "loading-keyframes": {
+          from: {
+            opacity: 0.5,
           },
-          "iphone-portrait": {
-            raw: `
+          to: {
+            opacity: 1,
+          },
+        },
+      },
+      animation: {
+        "fade-in": "opacity-keyframes .5s ease-in-out forwards",
+        loading: "loading-keyframes infinite",
+      },
+      screens: {
+        standalone: {
+          raw: "(display-mode: standalone)",
+        },
+        "iphone-portrait": {
+          raw: `
             (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait), 
             (device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait), 
             (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait), 
             (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait), 
             (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)
           `,
-          },
         },
       },
     },
