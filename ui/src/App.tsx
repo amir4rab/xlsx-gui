14bee247a2eefa08d775ce50234f37b78da932fc
Amir <60965542+amir4rab@users.noreply.github.com>
2024-05-03 07:32:10 -0400

    ♻️ Updates to the "main" and "app" components

--- ui/src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1 className="text-blue-300">Hello world!</h1>;
-}
-
-export default App;
---
+++ ui/src/app.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+// Components
+import CoreProvider from "@/components/core";
+import Hero from "@/components/hero";
+import FilePicker from "@/components/filePicker";
+import FileInterface from "@/components/fileInterface";
+
+function App() {
+  const [activeStage, setActiveState] = useState<
+    "hero" | "filePicker" | "fileInterface"
+  >("hero");
+
+  return (
+    <CoreProvider>
+      <header>
+        <h1 className="font-serif my-0 text-6xl sm:text-8xl font-bold">
+          Aurora
+        </h1>
+        <h2 className="my-0 mt-2 mb-6 font-light text-xl opacity-85">
+          Modify your excel files with ease!
+        </h2>
+      </header>
+      <Hero
+        isActive={activeStage === "hero"}
+        onProgress={setActiveState.bind(null, "filePicker")}
+      />
+      <FilePicker
+        isActive={activeStage === "filePicker"}
+        isVisible={
+          activeStage === "filePicker" || activeStage === "fileInterface"
+        }
+        onProgress={setActiveState.bind(null, "fileInterface")}
+      />
+      <FileInterface
+        isActive={activeStage === "fileInterface"}
+        isVisible={activeStage === "fileInterface"}
+        id="fileInterface"
+        reset={() => {}}
+      />
+    </CoreProvider>
+  );
+}
+
+export default App;
