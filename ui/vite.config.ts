5185e1d3a292c5accd924d1f44edab9575f4ad1e
Amir <60965542+amir4rab@users.noreply.github.com>
2024-05-03 02:58:54 -0400

    ♻️ Added TS paths

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "node:path";
+import { cwd } from "node:process";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(cwd(), "/src"),
+    },
+  },
   plugins: [react()],
-})
+});
