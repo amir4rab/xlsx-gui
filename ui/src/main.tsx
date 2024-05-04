420410ecf3b2b0920a3566c0344c3c756f631070
Amir <60965542+amir4rab@users.noreply.github.com>
2024-05-04 07:15:50 -0400

    ♻️ Minor visual fixes and improvements

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -4,7 +4,7 @@ import App from "./app.tsx";
 import "./base.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <main className="prose dark:prose-invert max-w-3xl p-8 pb-36 md:px-0 md:py-12 mx-auto">
+  <main className="prose dark:prose-invert max-w-4xl p-8 pb-36 md:px-0 md:py-12 mx-auto">
     <React.StrictMode>
       <App />
     </React.StrictMode>
